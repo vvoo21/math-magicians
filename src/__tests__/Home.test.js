7d7836f510cac7c9857ad22e7feb45e6a23e124e
Vanessa O <vaneolipa@gmail.com>
2022-12-07 18:15:08 -0500

    Add tests with jest

---
+++ src/__tests__/Home.test.js
@@ -0,0 +1,9 @@
+import renderer from 'react-test-renderer';
+import Home from '../components/Home';
+
+it('renders correctly', () => {
+  const home = renderer
+    .create(<Home />)
+    .toJSON();
+  expect(home).toMatchSnapshot();
+});
